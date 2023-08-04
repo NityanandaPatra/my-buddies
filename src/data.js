7c1825d4441de1e45412d5e73f9913137ef25dc5
NityanandaPatra <pnitananda2001@gmail.com>
2023-08-04 21:50:14 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,47 @@
+import {
+  nitya,
+  SHIVI,
+  RAJI,
+  NIT,
+  DIV,
+} from "./mint";
+
+const reviews = [
+  {
+    id: 1,
+    name: "NITYANADA PATRA",
+    job: "FRONTEND DEVELOPER",
+    image: nitya,
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "SHIVANI SAHU",
+    job: "UI/UX",
+    image: SHIVI,
+    text: " I designed the header section of the page, including the logo and the navigation bar.As for the Home.css file, my contribution focused on the layout and styling of the different components on the page.I worked closely with the UI/ UX of the project to ensure that the design and elements were consistent throughout the page.",
+  },
+  {
+    id: 3,
+    name: "DEBASIS MISHRA",
+    job: "BACKEND DEVELOPER",
+    image:DIV ,
+    text: "Whenever the user opens the link of our website, he or she gets landed to the sign-up page where the user has the option to sign up manually by entering his or her credentials like email id and password or he/she also has the option to sign up with Google.",
+  },
+  {
+    id: 4,
+    name: "SOM MOHANTY",
+    job: "FRONTEND DEVELOPER",
+    image: NIT,
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "RAJIV RATHA",
+    job: "BACKEND DEVELOPER",
+    image: RAJI,
+    text: "I had the opportunity to work with this team on a FITNESS APP project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+];
+
+export default reviews;
---
+++ src/mint/index.js
@@ -0,0 +1,14 @@
+import nitya from "./nitya.jpeg";
+import SHIVI from "./SHIVI.jpg";
+import RAJI from "./RAJI.jpg";
+import NIT from "./NIT.jpg";
+import DIV from "./DIV.jpg";
+
+export {
+    nitya,
+    SHIVI,
+    RAJI,
+    NIT,
+    DIV,
+};
+
